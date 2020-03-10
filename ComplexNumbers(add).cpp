12bab67be99c6479089e5015f4a223ae148a7627
bifenbecker <61775951+bifenbecker@users.noreply.github.com>
2020-03-10 11:36:55 +0300

    Complex_Numbers.Add
    Adding two complex numbers

---
+++ ComplexNumbers(add).cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct complex {
+	float re;
+	float im;
+
+};
+complex read() {
+	complex x;
+	cout << "Enter a complex number: " << endl;
+	cin >> x.re;
+	cin >> x.im;
+	return x;
+};
+void print(complex c) {
+	cout << endl << c.re;
+	if (c.im < 0) {
+		cout << c.im << "i ";
+	}
+	else {
+		cout << "+" << c.im << "i ";
+	}
+}
+complex add(complex a,complex b) {
+	complex c;
+	c.re = a.re + b.re;
+	c.im = a.im + b.im;
+	return c;
+}
+int main()
+{
+	
+	complex x1,x2;
+	x1 =  read();
+	x2 = read();
+	print(add(x1, x2));
+	return 0;
+}
+
